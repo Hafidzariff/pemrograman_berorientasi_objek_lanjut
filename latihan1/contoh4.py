43879472f8e027de7ac4e6a43f574720c1f16dc9
Hafidzariff <128382008+Hafidzariff@users.noreply.github.com>
2023-03-20 20:25:26 +0700

    Add files via upload

---
+++ latihan1/contoh4.py
@@ -0,0 +1,11 @@
+class Buku:
+    def __init__(self, judul, penulis):
+        self.judul = judul
+        self.penulis = penulis
+	
+    def info(self):
+        print(f"Judul: {self.judul}\nPenulis: {self.penulis}")
+        
+bukuA = Buku("Harry Potter and the Philosopher's Stone", "J.K. Rowling")
+bukuA.info()
+
